54859da0821e1a353e6f3111e3d2855338a5e1e7
uibeom96 <uibeom96@naver.com>
2021-12-23 00:18:54 +0900

    게시글 수정 페이지 로드시 현재 판매상태/카테고리상태 선택상태 되있게 구현

--- static/js/postedit.js
+++ static/js/postedit.js
@@ -1,1 +1,7 @@
-//test
+function changeSoldOut(value) {
+  document.querySelector(".sold_btn").value = value
+}
+
+function changeCategory(value) {
+  document.querySelector(".category_btn").value = value
+}
