c2354a20ee0c10a8e04ebca1a96bc6e4d623125a
clsrns111 <clsrns1111@gmail.com>
2021-12-24 14:43:23 +0900

    image upload 수정

--- static/js/post.js
+++ static/js/post.js
@@ -37,42 +37,47 @@ function change_btn() {
   let fileName = document.querySelector('.file_input').files;
   let files = document.querySelector('.file_input').files.length;
   let postImg = document.querySelector('.post_img_line');
-
-  let imgBox = document.createElement('div');
-  imgBox.classList.add('img-box');
-
   let file_count = document.querySelectorAll('.img-box > .img ').length;
 
   if (file_count < 5) {
-    imgBox.innerHTML = ``;
+    // imgBox.innerHTML = ``;
+
     for (let i = 0; i < files; i++) {
+      let imgBox = document.createElement('div');
+      imgBox.classList.add('img-box');
       let div = document.createElement('div');
       let img = document.createElement('img');
       let button = document.createElement('button');
       div.classList.add('img');
       img.src = URL.createObjectURL(input.files[i]);
-      button.type = "button"
-      
+      button.type = 'button';
+
       button.classList.add('img-cheking');
-      button.appendChild(img)
+      button.appendChild(img);
       div.appendChild(button);
       imgBox.appendChild(div);
       console.log(URL.createObjectURL(input.files[i]));
+      postImg.appendChild(imgBox);
     }
-    postImg.appendChild(imgBox);
 
-    let file_counting = document.querySelectorAll(
-      '.img-box > .img ',
-    ).length;
-
+    let file_counting = document.querySelectorAll('.img-box > .img ').length;
     data.innerText = `${file_counting}/5`;
-    
-    imgBox.onclick = () => {
-      imgBox.parentNode.removeChild(imgBox);
-      
-      let file_counting = document.querySelectorAll('.img-box > .img').length;
-      data.innerText = `${file_counting}/5`;
-    }
+
+    const images = document.querySelectorAll('.img-box');
+
+    images.forEach(image =>
+      image.addEventListener('click', e => {
+        image.remove();
+        let file_counting = document.querySelectorAll('.img-box > .img').length;
+        data.innerText = `${file_counting}/5`;
+      }),
+    );
+
+    // imgBox.onclick = () => {
+    //   imgBox.parentNode.removeChild(imgBox);
+    //   let file_counting = document.querySelectorAll('.img-box > .img').length;
+    //   data.innerText = `${file_counting}/5`;
+    // };
   } else {
     alert('이미지는 최대 5개까지 첨부할 수 있어요');
   }
